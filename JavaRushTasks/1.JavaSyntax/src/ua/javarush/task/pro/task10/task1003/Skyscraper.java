786979fad30d50f67287dc06f6530fb8716244b0
Shlyakhetko Vitaliy <vit_shl@ukr.net>
2023-03-14 21:38:18 +0200

    task1003 Scyscrapper with 2constructors

---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task10/task1003/Skyscraper.java
@@ -0,0 +1,26 @@
+package ua.javarush.task.pro.task10.task1003;
+
+/* 
+Збудуймо новий бізнес-комплекс JavaRush Business Center
+*/
+
+public class Skyscraper {
+    private int floorsCount;
+    private String developer;
+
+    public Skyscraper(){
+        floorsCount=5;
+        developer = "JavaRushDevelopment";
+    }
+    //напишіть тут ваш код
+
+    public Skyscraper(int floorsCount, String developer) {
+        this.floorsCount = floorsCount;
+        this.developer = developer;
+    }
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+        Skyscraper skyscraperUnknown = new Skyscraper(50, "Невідомо");
+    }
+}
