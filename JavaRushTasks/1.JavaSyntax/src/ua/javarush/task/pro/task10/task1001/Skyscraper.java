4b17233d74ec99dbf6b3aa549d284fc08ff51db8
Shlyakhetko Vitaliy <vit_shl@ukr.net>
2023-03-14 19:58:48 +0200

    task1002 Scyscrapper

---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,18 @@
+package ua.javarush.task.pro.task10.task1001;
+
+/* 
+Створімо хмарочос
+*/
+
+public class Skyscraper {
+
+    public Skyscraper(){
+        System.out.println("Хмарочос побудовано.");
+    }
+    //напишіть тут ваш код
+
+    public static void main(String[] args) {
+      Skyscraper skyscraper = new Skyscraper();
+        //напишіть тут ваш код
+    }
+}
