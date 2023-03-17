b666bc062b4ca4883ead7652ddb703102e3f3cec
Shlyakhetko Vitaliy <vit_shl@ukr.net>
2023-03-17 20:50:25 +0200

    task1007_08 Solve task

---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task10/task1007/Bugatti.java
@@ -0,0 +1,36 @@
+package ua.javarush.task.pro.task10.task1007;
+
+/* 
+Чи з дахом, чи без нього — от питання
+*/
+
+public class Bugatti {
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    //напишіть тут ваш код
+}
