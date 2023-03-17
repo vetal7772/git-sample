9bccfb0d653e6a3e4495d737a5296e149fd80133
Shlyakhetko Vitaliy <vit_shl@ukr.net>
2023-03-17 20:04:57 +0200

    task1006 Solve task about super

---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task10/task1006/GasCar.java
@@ -0,0 +1,8 @@
+package ua.javarush.task.pro.task10.task1006;
+
+public class GasCar extends Car {
+
+    public GasCar() {
+        super("GasCar");//напишіть тут ваш код
+    }
+}
---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task10/task1006/Solution.java
@@ -0,0 +1,13 @@
+package ua.javarush.task.pro.task10.task1006;
+
+/* 
+Хто тут спадкоємець?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        HybridCar hybridCar = new HybridCar();
+        ElectricCar electricCar = new ElectricCar();
+        GasCar gasCar = new GasCar();
+    }
+}
