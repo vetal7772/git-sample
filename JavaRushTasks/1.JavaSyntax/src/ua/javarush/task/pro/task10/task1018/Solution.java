879ee0e313ae11dd559e725ec0972a650f58ff31
Shlyakhetko Vitaliy <vit_shl@ukr.net>
2023-03-18 20:35:09 +0200

    task1017_21 Solve task

---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task10/task1018/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.task.pro.task10.task1018;
+
+/* 
+Гетер і сетер в ділі
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Coordinate coordinate = new Coordinate();
+        coordinate.setX(15);
+        coordinate.setY(25);
+        System.out.println(coordinate.getX());
+        System.out.println(coordinate.getY());
+    }
+}
---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,11 @@
+package ua.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    public Computer(){
+        new Keyboard();
+        new Monitor();
+        new Mouse();
+        new SystemUnit();
+    }
+
+}
