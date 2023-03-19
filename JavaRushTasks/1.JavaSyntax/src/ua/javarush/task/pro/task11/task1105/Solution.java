4aeb2f32f36d159471d5d0c3cdbd2274d9f4e12d
Shlyakhetko Vitaliy <vit_shl@ukr.net>
2023-03-19 23:17:45 +0200

    task1101_1109 lvl_14 Solve task

---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task11/task1105/Solution.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task11.task1105;
+
+/* 
+Віконні роботи
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.printSize();
+        window.changeSize(4, 5);
+        window.printSize();
+    }
+}
---
+++ JavaRushTasks/1.JavaSyntax/src/ua/javarush/task/pro/task11/task1106/Car.java
@@ -0,0 +1,26 @@
+package ua.javarush.task.pro.task11.task1106;
+
+/* 
+Однакові машини
+*/
+
+public class Car {
+    private  String modelName;
+    private int speed;
+
+    public  String getModelName() {
+        return modelName;
+    }
+
+    public  void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
