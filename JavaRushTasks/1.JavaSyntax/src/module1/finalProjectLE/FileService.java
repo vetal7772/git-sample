42b3496ef1ca18ac85c0065eb146333c92eb12ab
Shlyakhetko Vitaliy <vit_shl@ukr.net>
2023-05-20 20:44:11 +0300

    module 1 final task v1.0

---
+++ JavaRushTasks/1.JavaSyntax/src/module1/finalProjectLE/FileService.java
@@ -0,0 +1,48 @@
+package module1.finalProjectLE;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileService {
+
+    public boolean isFileExists(Path filepath) {
+        return Files.exists(filepath);
+    }
+
+    public String read(Path filePath) throws IOException {
+
+        String src = new String(Files.readAllBytes(filePath));
+        return src;
+    }
+
+    public void writeEncryptedFile(String filePath, String text) throws IOException {
+        writeFile(filePath, text, "[ENCRYPTED]");
+    }
+
+
+    public void writeFile(String filePath, String fileContent, String status) throws IOException {
+        Path path = Paths.get(filePath);
+
+        String fileName = path.getFileName().toString();
+        String fileExtension = " ";
+        int extensionIndex = fileName.lastIndexOf('.');
+        if (extensionIndex > 0) {
+            fileExtension = fileName.substring(extensionIndex);
+            fileName = fileName.substring(0, extensionIndex);
+            String newFileName = fileName + status + fileExtension;
+            Path newFilePath = path.resolveSibling(newFileName);
+            Files.writeString(newFilePath, fileContent);
+        }
+    }
+
+
+    public void writeDecryptedFile(String filePath, String text) throws IOException {
+        writeFile(filePath, text, "[DECRYPTED]");
+    }
+
+    public void bruteForced(String filePath, String text) throws IOException {
+        writeFile(filePath, text, "[BRUTE_FORCE]");
+    }
+}
---
+++ JavaRushTasks/1.JavaSyntax/src/module1/finalProjectLE/Main.java
@@ -0,0 +1,12 @@
+package module1.finalProjectLE;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Runner runner = new Runner();
+        runner.run(args);
+
+    }
+}
