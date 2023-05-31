356f0e9a681b153b922fb804259621dff4972f35
Shlyakhetko Vitaliy <vit_shl@ukr.net>
2023-05-31 13:05:51 +0300

    last May 2023 edition

---
+++ 1.JavaSyntax/src/test.project/module1_final_projectLE/Main.java
@@ -0,0 +1,12 @@
+package test.project.module1_final_projectLE;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Runner runner = new Runner();
+        runner.run(args);
+
+    }
+}
